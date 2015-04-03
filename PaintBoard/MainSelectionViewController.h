62dbc603792f611b50b6a62376b20bed506554f4
Changkun Zhao <changkunzhao@Changkuns-MacBook-Air.local>
2015-04-03 14:30:35 -0400

    implements most basic functions

---
+++ PaintBoard/MainSelectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainSelectionViewController.h
+//  PaintBoard
+//
+//  Created by Changkun Zhao on 4/2/15.
+//  Copyright (c) 2015 Changkun Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainSelectionViewController : UIViewController
+
+@end
---
+++ PaintBoard/PBColors.h
@@ -0,0 +1,13 @@
+//
+//  PBColors.h
+//  PaintBoard
+//
+//  Created by Changkun Zhao on 4/3/15.
+//  Copyright (c) 2015 Changkun Zhao. All rights reserved.
+//
+
+#ifndef PaintBoard_PBColors_h
+#define PaintBoard_PBColors_h
+
+
+#endif

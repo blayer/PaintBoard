fedc4109c4f45110afc2efbcc71dee23e6c822ec
Changkun Zhao <changkunzhao@Changkuns-MacBook-Air.local>
2015-04-03 19:32:55 -0400

    adding color picker

---
+++ PaintBoard/ColorPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  ColorPickerViewController.h
+//  PaintBoard
+//
+//  Created by Changkun Zhao on 4/3/15.
+//  Copyright (c) 2015 Changkun Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ColorPickerViewController : UIViewController
+
+@end
---
+++ PaintBoard/ColorPickerViewController.m
@@ -0,0 +1,37 @@
+//
+//  ColorPickerViewController.m
+//  PaintBoard
+//
+//  Created by Changkun Zhao on 4/3/15.
+//  Copyright (c) 2015 Changkun Zhao. All rights reserved.
+//
+
+#import "ColorPickerViewController.h"
+
+@interface ColorPickerViewController ()
+
+@end
+
+@implementation ColorPickerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
